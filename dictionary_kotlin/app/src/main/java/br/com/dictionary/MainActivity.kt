0452dfa30f7ca101d4a92b763569529aa705b032
Jadiel Santana <jadiel.bsantana@gmail.com>
2023-03-20 09:59:51 -0300

    🧩: Create Kotlin project

---
+++ dictionary_kotlin/app/src/main/java/br/com/dictionary/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.dictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fab: View = findViewById(R.id.fab_add_word)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
+    }
+}
